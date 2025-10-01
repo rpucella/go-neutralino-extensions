6804070d422782b591b6c79a14cc74066ded1209
Riccardo Pucella <riccardo.pucella@gmail.com>
2025-10-01 01:22:04 -0400

    Add sqlite example

---
+++ examples/sqlite/go.mod
@@ -0,0 +1,16 @@
+module github.com/rpucella/go-neutralino-extensions/examples/sqlite
+
+go 1.23.3
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.32
+	github.com/rpucella/go-neutralino-extensions v0.0.0-20251001050614-a8cde5c425f5
+)
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
+
+// Redirect module to use the one in this exact repo.
+replace github.com/rpucella/go-neutralino-extensions => ../..
---
+++ examples/sqlite/sqlite.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"errors"
+	"github.com/rpucella/go-neutralino-extensions"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+
+	connInfo, err := neutralinoext.ReadConnInfo(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := connInfo.StartMessageLoop(processMsg); err != nil {
+		panic(err)
+	}
+}
+
+func extractStrings(m map[string]any, keys []string) ([]string, error) {
+	result := make([]string, len(keys))
+	for i, key := range keys {
+		ifc, ok := m[key]
+		if !ok {
+			return nil, fmt.Errorf("no key %s", key)
+		}
+		s, ok := ifc.(string)
+		if !ok {
+			return nil, fmt.Errorf("not a string %v", ifc)
+		}
+		result[i] = s
+	}
+	return result, nil
+}
+
+func processMsg(event string, data any) (map[string]any, error) {
+	if event != "query" {
+		return nil, nil
+	}
+	dataObj, ok := data.(map[string]any)
+	if !ok {
+		return nil, errors.New("data not an object")
+	}
+	params, err := extractStrings(dataObj, []string{"database", "query"})
+	if err != nil {
+		return nil, err
+	}
+	rows, err := queryDatabase(params[0], params[1])
+	if err != nil {
+		return nil, err
+	}
+	result := make(map[string]any)
+	result["rows"] = rows
+	return result, nil
+}
+
+func queryDatabase(database string, query string) ([][]any, error) {
+	db, err := sql.Open("sqlite3", database)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open db file: %w", err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("cannot query: %w", err)
+	}
+	cols, err := rows.Columns()
+	if err != nil {
+		return nil, fmt.Errorf("cannot get columns: %w", err)
+	}
+	result := make([][]any, 0)
+	for rows.Next() {
+		row := make([]any, len(cols))
+		rowAddrs := make([]any, len(cols))
+		for i := range row {
+			rowAddrs[i] = &row[i]
+		}
+		err = rows.Scan(rowAddrs...)
+		if err != nil {
+			return nil, fmt.Errorf("error scanning row: %w", err)
+		}
+		result = append(result, row)
+	}
+	return result, nil
+}
